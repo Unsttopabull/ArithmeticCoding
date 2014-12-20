c8be3b15f622ce270aeb00906cfc1ab92955994a
Martin Kraner <martinkraner@outlook.com>
2014-12-20 16:05:52 +0100

    Dodal prikazovanje kodiranja/dekodiranja v ACDemo.

--- PrikazDelovanja Konzola/Program.cs
+++ PrikazDelovanja Konzola/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using ArtimeticniKodirnik;
 using ArtimeticniKodirnik.Dekodiranje;
 using ArtimeticniKodirnik.Kodiranje;
+using ConsoleTables.Core;
 
 namespace AritmeticniKodirnik.Konzola.Demo {
 
     internal class Program {
+        private static StringBuilder _rezultat;
+        private static ConsoleTable _tabela;
+
         private static void Help() {
             Console.WriteLine("Program AC - coded by {0}", "Martin Kraner E5020649");
             Console.WriteLine("Usage: ACDemo -F [-E/-D] inputFile");
@@ -17,9 +22,11 @@ namespace AritmeticniKodirnik.Konzola.Demo {
         }
 
         private static void Main(string[] args) {
-            if (args.Length == 3) {
+            if (args.Length != 3) {
                 Help();
             }
+
+            _rezultat = new StringBuilder();
 
             switch (args[0].ToUpperInvariant()) {
                 case "-F":
@@ -55,13 +62,73 @@ namespace AritmeticniKodirnik.Konzola.Demo {
         }
 
         private static void Kodiraj(byte[] data) {
+            _tabela = new ConsoleTable("Symbol", "(L,H)", "Step", "(nL, nH)", "Operations + Output", "E3Cnt");
+
             Kodirnik k = new Kodirnik(StBitov.Bit64, true);
-            data = k.Kodiraj(data);
+            k.SimbolZakodiran += SimbolZakodiran;
+            k.TabelaGenerirana += TabelaGenerirana;
+
+            k.Kodiraj(data);
+
+            Console.WriteLine(_tabela.ToString());
+            Console.WriteLine(_rezultat.ToString());
         }
 
         private static void Dekodiraj(byte[] data) {
+            _tabela = new ConsoleTable("V", "(L,H)", "Step", "S", "Symbol", "(nL, nH)", "Operations");
+
             Dekodirnik d = new Dekodirnik(true);
-            data = d.Dekodiraj(data);
+            d.TabelaGenerirana += TabelaGenerirana;
+            d.SimbolDekodiran += SimbolDekodiran;
+
+            d.Dekodiraj(data);
+
+            Console.WriteLine(_tabela.ToString());
+            Console.WriteLine(_rezultat.ToString());
+        }
+
+        private static void TabelaGenerirana(IList<Simbol> tabela) {
+            ConsoleTable consoleTable = new ConsoleTable("Symbol", "Freq", "l(x)", "h(x)");
+            
+            foreach (Simbol s in tabela.Where(s => s != null)) {
+                consoleTable.AddRow(new string((char) s.Vrednost, 1), s.Frekvenca, s.SpodnjaMeja, s.ZgornjaMeja);
+            }
+            consoleTable.Write();
+            Console.WriteLine();
+        }
+
+        private static void SimbolZakodiran(byte simbol, ulong spMeja, ulong zgMeja, ulong korak, ulong novaSpMeja, ulong novaZgMeja, int e3Count, params Operacija[] operacije) {
+            if (e3Count == -1) {
+                _tabela.AddRow("", "", "", "", string.Join<Operacija>("; ", operacije), "");
+            }
+            else {
+                _tabela.AddRow(
+                    new string((char) simbol, 1),
+                    string.Format("({0}, {1})", spMeja, zgMeja),
+                    korak,
+                    string.Format("({0}, {1})", novaSpMeja, novaZgMeja),
+                    string.Join<Operacija>("; ", operacije),
+                    e3Count
+                );
+            }
+
+            foreach (Operacija operacija in operacije) {
+                _rezultat.Append(operacija.Izhod);
+            }
+        }
+
+        private static void SimbolDekodiran(string polje, ulong spMeja, ulong zgMeja, ulong korak, ulong vrednostSymb, byte simbol, ulong novaSpMeja, ulong novaZgMeja, params string[] operacije) {
+            _tabela.AddRow(
+                polje,
+                string.Format("({0}, {1})", spMeja, zgMeja),
+                korak,
+                vrednostSymb,
+                new string((char)simbol, 1),
+                string.Format("({0}, {1})", novaSpMeja, novaZgMeja),
+                string.Join("; ", operacije)
+            );
+
+            _rezultat.Append((char) simbol);
         }
     }
 
